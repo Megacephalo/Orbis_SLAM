657433a86c4b22a44ae94572db72f5ac442f62ad
Charly Huang <charly.charlongo@gmail.com>
2025-10-14 12:16:06 +0800

    Implement the main SLAM pipeline, or add the essentail code to grab new frames from the stereo camera based on the Orbis' ZED wrapper

--- app/orbis_slam_main.cpp
+++ app/orbis_slam_main.cpp
@@ -1,10 +1,28 @@
 #include <cstdio>
+#include <iostream>
+#include <iomanip>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
 
-  printf("hello world orbis_slam package\n");
-  return 0;
+
+#include "orbis_slam/zed_setup_help_utils.h"
+#include "orbis_slam/zed_wrapper.h"
+
+int main(int argc, char ** argv) {
+    std::cout << "Launching get_camera_parameters" << std::endl;
+
+    Orbis::ZEDWrapper zed_wrapper;
+    bool camera_ready = zed_wrapper.setup(argc, argv);
+    if ( ! camera_ready ) {
+        std::cerr << "Camera setup failed. Exit." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    while (zed_wrapper.hasNewFrame()) {
+        zed_wrapper.grabFrame();
+
+        cv::imshow("ZED Stream", zed_wrapper.getStereoImage());
+        cv::waitKey(1);
+    }
+
+    return EXIT_SUCCESS;
 }
