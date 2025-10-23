42909c908a2c8c2b584bab267c87e59382566f02
Charly Huang <charly.charlongo@gmail.com>
2025-10-23 10:54:48 +0800

    Add option to switch off the Rviz and load in a custom Rviz config file for this package to be brought up in a separate Rviz2

--- launch/orbis_slam_sensor_only.py
+++ launch/orbis_slam_sensor_only.py
@@ -2,15 +2,31 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     # Get the package directory
     pkg_orbis_slam = get_package_share_directory('orbis_slam')
+
+    enable_rviz_arg = DeclareLaunchArgument(
+        'enable_rviz',
+        default_value='false',
+        description='Enable RViz visualization'
+    )
+    rviz_config_file = DeclareLaunchArgument(
+        'rviz_config_file',
+        default_value=PathJoinSubstitution([
+            pkg_orbis_slam,
+            'launch',
+            'sensor_only_view.rviz'
+        ]),
+        description='Full path to the RViz config file to use'
+    )
 
     zed2i_model_bringup = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
@@ -19,7 +35,10 @@ def generate_launch_description():
                 'launch',
                 'zed_camera.launch.py'
             ])
-        ])
+        ]),
+        launch_arguments={
+            'enable_rviz': LaunchConfiguration('enable_rviz')
+        }.items()
     )
     
     
@@ -39,9 +58,20 @@ def generate_launch_description():
             'cam_pointcloud_topic'  : 'zed/point_cloud'
         }]
     )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='Orbis_SLAM_rviz2',
+        output='screen',
+        arguments=["-d", LaunchConfiguration('rviz_config_file')],
+    )
     
     # Create and return the launch description
     return LaunchDescription([
+        enable_rviz_arg,
+        rviz_config_file,
         zed2i_model_bringup,
-        orbis_slam_node
+        orbis_slam_node,
+        rviz_node
     ])
